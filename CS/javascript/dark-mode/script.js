f573845fa13ecdfc175ba34ea4acc77606e54ca8
Doukaza <cyrushenry430@gmail.com>
2024-10-22 17:52:31 -0400

    Update script.js

--- CS/javascript/dark-mode/script.js
+++ CS/javascript/dark-mode/script.js
@@ -1,3 +1,11 @@
 function darkMode() {
-// Your code here.
+const variable_darkmode = document.body;
+  function darkmode(){
+    if(variable_darkmode.style.backgroundColor === 'white'){
+      variable_darkmode.style.backgroundColor = 'black';
+        variable_darkmode.style.backgroundColor = 'white';
+    }
+      else{
+      variable_darkmode.style.backgroundColor = 'white';
+        variable_darkmode.style.color = 'black';
 }
