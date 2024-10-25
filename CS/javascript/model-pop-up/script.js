0c9e8db53487ffeb15eaa1e7fc03ff3dddb67e85
Doukaza <cyrushenry430@gmail.com>
2024-10-25 18:27:20 -0400

    Update script.js

--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,3 +1,16 @@
-function modelPopUp() {
-    // Your code here.
-}   
+document.addEventListener("DOMContentLoaded", () => {
+     const modal = document.getElementById("myModal");
+     const openbutton = document.getElementById("openModal");
+     const closebutton = document.getElementById("closeModal");
+     openbutton.addEventListener("click", () => {
+        modal.style.display = "block";
+    });
+     closebutton.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+     window.addEventListener("click", (event) => {
+         if (event.target == modal){
+            modal.style.display = "none";{    
+         }
+     }): 
+}):
