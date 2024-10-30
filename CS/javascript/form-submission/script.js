009cc1d299a6ca21e80f22db768a1837518d13bd
Doukaza <cyrushenry430@gmail.com>
2024-10-30 17:02:19 -0400

    Update script.js

--- CS/javascript/form-submission/script.js
+++ CS/javascript/form-submission/script.js
@@ -1,3 +1,9 @@
-function formSubmission() {
-    // Your code here.
+ document.addEventListener ("DOMContentLoaded", () => {
+    const form = document. getElementById("userInfo")
+    form. addEventListener ('submit', function (event) ){
+    event. preventDefault)
+    name = document.getElementById("userInfo")
+    name = document.getElementById("")
+    name = document.getElementById("userInfo")
+if
 }
