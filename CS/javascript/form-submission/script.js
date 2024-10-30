dc5320be90a44a5b67f0fa260deb9a91e74797e4
Doukaza <cyrushenry430@gmail.com>
2024-10-30 18:18:51 -0400

    Update script.js

--- CS/javascript/form-submission/script.js
+++ CS/javascript/form-submission/script.js
@@ -1,9 +1,12 @@
- document.addEventListener ("DOMContentLoaded", () => {
-    const form = document. getElementById("userInfo")
-    form. addEventListener ('submit', function (event) ){
-    event. preventDefault)
-    name = document.getElementById("userInfo")
-    name = document.getElementById("")
-    name = document.getElementById("userInfo")
-if
-}
+document.addEventListener ("DOMContentLoaded", () => {
+     const form = document.getElementById("userInfo")
+     form.addEventListener ('submit', function (event) ){
+          event.preventDefault)
+          name = document.getElementById("userInfo")
+          email = document.getElementById("email")
+          message = document.getElementById("message")
+          if (name && email && message) {
+               alert( Form submitted successfully!\name: S{name)\nEmail: S{email)\nMessage: S{message)');
+          }
+          else{
+          }
