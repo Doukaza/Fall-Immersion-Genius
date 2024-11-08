5b415e26c834432c79f970cdb7f1310cdc361b7d
Doukaza <cyrushenry430@gmail.com>
2024-11-08 17:14:41 -0500

    Update script.js

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,6 +1,6 @@
 document.getElementById("MyButton").addEventListener("'click', function (){
   essay = document.createElement('img');
-  essay.textContent = "https://i.scdn.co/image/ab67616d0000b273090b1f9557a6d23c0817bd88";
+  essay.<img src="" = "img";
   document.body.appendChild(essay);
 
 });
