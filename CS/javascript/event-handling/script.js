2af3707f185f74c43a9c85336370de3eb68f2b27
Doukaza <cyrushenry430@gmail.com>
2024-11-08 16:49:45 -0500

    Update script.js

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,6 +1,6 @@
-document. getElementById("myButton").addEventListener("'click', function (){
-  paragraph = document. createElement ('p');
-  paragraph. textContent = "text";
-  document. body. appendChild (paragraph);
+document.getElementById("myButton").addEventListener("'click', function (){
+  paragraph = document.createElement ('img');
+  paragraph.textContent = "text";
+  document.body.appendChild (paragraph);
 
 });
