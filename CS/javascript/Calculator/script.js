9a3eec07aa07119ecc0679345cdebad4b9992573
Doukaza <cyrushenry430@gmail.com>
2024-11-21 18:23:08 -0500

    Update script.js

--- CS/javascript/Calculator/script.js
+++ CS/javascript/Calculator/script.js
@@ -1,1 +1,23 @@
+const display = document.getElementById('display');
 
+function appendToDisplay(value) {
+    display.value += value; 
+}
+
+function toggleSign() {
+    if (display.value) {
+        display.value = String(parseFloat(display.value) * -1); 
+    }
+}
+
+function clearDisplay() {
+    display.value = ''; 
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value); 
+    } catch (error) {
+        display.value = 'Error'; 
+    }
+}
